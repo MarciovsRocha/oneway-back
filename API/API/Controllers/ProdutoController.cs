ca1f064e8ed770613e024c6407d193df4b112d69
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2024-03-02 18:18:14 -0300

    criada API WEB Rest e adicionado exemplo de produto

---
+++ API/API/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProdutoController : ControllerBase
+{
+    public ProdutoController()
+    {
+    }
+
+    [HttpGet(Name = "GetProduto")]
+    public IEnumerable<Produto> Get()
+    {
+        return Enumerable.Range(1, 10).Select(index=> new Produto{id = Random.Shared.Next(1,100), preco = Random.Shared.NextDouble()}).ToArray();
+    }
+}
