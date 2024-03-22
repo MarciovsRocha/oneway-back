9d69ac0f0ae808aca53199b003511c69abdff2c1
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2024-03-22 06:46:26 -0300

    .

---
+++ API/OneWay.EmpresaAPI/GetAllProducts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace OneWay.EmpresaAPI;
+
+public static class GetAllProducts
+{
+    [FunctionName("GetAllProducts")]
+    public static async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products")] HttpRequest req)
+    {
+        var producList = Enumerable.Range(1, 10).Select(
+            index=> new Product
+            {
+                Id = Random.Shared.Next(1,100), 
+                Price = Random.Shared.NextDouble()
+            }).ToArray();
+        return new OkObjectResult(producList);
+    }
+}
---
+++ API/OneWay.EmpresaAPI/Model/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace OneWay.EmpresaAPI;
+
+public class Product
+{
+    public int Id { get; set; }
+    public double Price { get; set; }
+}
